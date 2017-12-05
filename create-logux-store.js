8814f5213a7bd6197807c761a2af9814351a54a3
Nikolay Govorov <nikolay_govorov@bk.ru>
2017-12-05 10:24:37 +0300

    Add structure

--- create-logux-store.js
+++ create-logux-store.js
@@ -7,6 +7,7 @@ function createLoguxStore (config) {
   }
 
   var client = new CrossTabClient(config)
+  var log = client.log
 
   function LoguxStore (options) {
     Store.call(this, options)
@@ -18,6 +19,43 @@ function createLoguxStore (config) {
   LoguxStore.prototype = Object.create(Store.prototype)
 
   LoguxStore.prototype.constructor = LoguxStore
+
+  LoguxStore.prototype.commit.local = commit.bind(LoguxStore, ['local'])
+
+  LoguxStore.prototype.commit.crossTab = commit.bind(LoguxStore, ['crossTab'])
+
+  LoguxStore.prototype.commit.sync = commit.bind(LoguxStore, ['sync'])
+
+  log.on('preadd', function () {})
+
+  log.on('add', function () {})
+
+  client.on('add', function () {})
+
+  client.on('clean', function () {})
+
+  client.sync.on('state', function () {})
+
+  function commit (type, mutationName, meta) {
+    if (!meta) meta = {}
+    if (!meta.reasons) meta.reasons = []
+
+    switch (type) {
+      case 'local': {
+        meta.tab = client.id
+
+        break
+      }
+      case 'sync': {
+        meta.sync = true
+        meta.reasons.push('waitForSync')
+
+        break
+      }
+    }
+
+    log.add(mutationName, meta)
+  }
 
   return LoguxStore
 }
